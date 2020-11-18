0d6a41ad7aad41377e73bbc691b35c8fa2f5c39e
student <student@Aurora.fritz.box>
2020-11-18 11:17:38 +0100

    Add input canvas and neuron connection lines

--- kohonen.js
+++ kohonen.js
@@ -16,16 +16,39 @@ for (i=0;i<8;i++) {
 
 console.log(network);
 
-canvas = document.getElementById("neurons");
-ctx = canvas.getContext('2d');
-ctx.fillStyle = "lavender";
-ctx.fillRect(0,0,canvas.width,canvas.height);
+canvas1 = document.getElementById("inputs");
+ctx1= canvas1.getContext('2d');
+ctx1.fillStyle = "navajowhite";
+ctx1.fillRect(0,0,canvas1.width,canvas1.height);
+
+canvas2 = document.getElementById("neurons");
+ctx2 = canvas2.getContext('2d');
+ctx2.fillStyle = "lavender";
+ctx2.fillRect(0,0,canvas2.width,canvas2.height);
 
 for (i=0;i<8;i++) {
   for (j=0;j<8;j++) {
-    ctx.beginPath();
-    ctx.arc(2*network[i][j].w1,2*network[i][j].w2,5,0,2*Math.PI,true);
-    ctx.stroke(); 
+    ctx2.beginPath();
+    ctx2.arc(network[i][j].w1,network[i][j].w2,2,0,2*Math.PI,true);
+    ctx2.stroke(); 
+  }
+}
+
+for (i=0;i<8;i++) {
+  for (j=0;j<8;j++) {
+    if (i<7) {
+      ctx2.strokeStyle = 'rgba(0.1,0.1,0.3,0.6)';
+      ctx2.beginPath();
+      ctx2.moveTo(network[i][j].w1,network[i][j].w2);
+      ctx2.lineTo(network[i+1][j].w1,network[i+1][j].w2);
+      ctx2.stroke();
+    }
+    if (j<7) {
+      ctx2.beginPath();
+      ctx2.moveTo(network[i][j].w1,network[i][j].w2);
+      ctx2.lineTo(network[i][j+1].w1,network[i][j+1].w2);
+      ctx2.stroke();
+    }
   }
 }
 /*
